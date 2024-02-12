3a2d1da7f77f70360bcd1cb6c0d8f08e79c1034f
Administrator <admin@example.com>
2024-02-12 14:34:26 +0100

    init repo

---
+++ src/components/user-test.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+export default class UserTest extends Component{
+
+    render(){
+        return (
+            <p>User working!</p>
+        );
+    }
+}
